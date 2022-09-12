61777fa45dfbd243f0e0f657bb8a6e94a2c62610
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-12 23:55:59 +0300

    add phone function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 
 const desktop = () =>{
     document.getElementsByClassName("page-body")[0].style.width ="100%";
@@ -41,6 +40,29 @@ const tablet = () =>{
     window.onscroll = function() {scrollFunctionTablet()};
 }
 
+const phone = () =>{
+    document.getElementsByClassName("page-body")[0].style.width ="40%";
+    document.getElementsByClassName("navbar")[0].style.width ="40%";
+    document.getElementsByClassName("footer")[0].style.width ="40% ";
+    document.getElementsByClassName("main-footer")[0].style.flexDirection="column";
+    document.getElementsByClassName("main-footer")[0].style.alignItems="center";
+
+
+    function scrollFunctionPhone() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+          document.getElementById("navbar").style.transitionDuration = "0.25s"
+          document.getElementById("navbar").style.padding = "20px 0px";
+        } else {
+          document.getElementById("navbar").style.transitionDuration = "0.25s"
+          document.getElementById("navbar").style.padding = "40px 0px";
+        }
+      }
+    
+    window.onscroll = function() {scrollFunctionPhone()};
+}
+
 document.getElementsByClassName("desktop")[0].addEventListener("click", desktop )
 
 document.getElementsByClassName("tablet")[0].addEventListener("click", tablet )
+
+document.getElementsByClassName("phone")[0].addEventListener("click", phone )
