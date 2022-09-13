1effd5c79e1955d0de0da9b5040d38c72f34c8db
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-13 21:26:26 +0300

    add javascript to page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
+// Desktop buttons
 const desktop = () =>{
+
+  //Desktop style
     document.getElementsByClassName("page-body")[0].style.width ="100%";
     document.getElementsByClassName("navbar")[0].style.width ="100%";
     document.getElementsByClassName("footer").style.width ="100%";
@@ -9,6 +11,7 @@ const desktop = () =>{
     document.getElementsByClassName("main-footer")[0].style.removeProperty("flex-direction")
     document.getElementsByClassName("main-footer")[0].style.removeProperty("align-items")
 
+   //Scroll function
     function scrollFunctionDesktop() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
           document.getElementById("navbar").style.transitionDuration = "0.25s"
@@ -22,7 +25,10 @@ const desktop = () =>{
 
 }
 
+// Tablet buttons
 const tablet = () =>{
+
+    //Tablet style
     document.getElementsByClassName("page-body")[0].style.width ="60%";
     document.getElementsByClassName("navbar")[0].style.width ="60%";
     document.getElementsByClassName("footer")[0].style.width ="60% ";
@@ -34,7 +40,7 @@ const tablet = () =>{
     document.getElementsByClassName("main-footer")[0].style.flexDirection="column";
     document.getElementsByClassName("main-footer")[0].style.alignItems="center";
 
-
+   //Scroll function
     function scrollFunctionTablet() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
           document.getElementById("navbar").style.transitionDuration = "0.25s"
@@ -48,7 +54,10 @@ const tablet = () =>{
     window.onscroll = function() {scrollFunctionTablet()};
 }
 
+// Phone buttons
 const phone = () =>{
+
+  // Phone style
     document.getElementsByClassName("page-body")[0].style.width ="40%";
     document.getElementsByClassName("navbar")[0].style.width ="40%";
     document.getElementsByClassName("footer")[0].style.width ="40% ";
@@ -59,7 +68,7 @@ const phone = () =>{
     document.getElementsByClassName("main-footer")[0].style.flexDirection="column";
     document.getElementsByClassName("main-footer")[0].style.alignItems="center";
 
-
+   //Scroll function
     function scrollFunctionPhone() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
           document.getElementById("navbar").style.transitionDuration = "0.25s"
@@ -73,8 +82,28 @@ const phone = () =>{
     window.onscroll = function() {scrollFunctionPhone()};
 }
 
+// Change pages
 document.getElementsByClassName("desktop")[0].addEventListener("click", desktop )
-
 document.getElementsByClassName("tablet")[0].addEventListener("click", tablet )
-
 document.getElementsByClassName("phone")[0].addEventListener("click", phone )
+
+// Api call function
+const sendData = async () => {
+
+  const res = await fetch("")
+  const data = await res.json();
+
+  return ''
+}
+
+// Send data to database
+document.getElementById("submit").addEventListener("click", () => {
+
+  // Get user input
+  const name = document.getElementById("name")
+  const email = document.getElementById("email")
+  const phone = document.getElementById("phone")
+  const message = document.getElementById("message")
+
+
+})
